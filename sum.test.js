cb311ce9834a3c3178055d1ea34774a8dcbb35fd
SwamiSankar <swamisankar2008@gmail.com>
2022-06-05 20:33:30 +0530

    First Refactor and added one test suite

--- sum.test.js
+++ sum.test.js
@@ -1,5 +1,13 @@
 const sum = require('./sum');
 
+test('adds no string to give 0', () => {
+  expect(sum('')).toEqual(0);
+});
+
+test('adds one number gives the same number', () => {
+  expect(sum('1')).toEqual(1);
+});
+
 test('adds 1,2 to equal 3', () => {
   expect(sum('1,2')).toEqual(3);
 });
